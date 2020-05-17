f3f6a7c1d8e5e14a978caf2d0e907046baeba8bc
Toby Brock <toby.brock@babylonhealth.com>
2020-05-18 00:27:58 +0100

    Begun implementing generateRooms function

---
+++ src/components/MapRenderer/__tests__/utils.spec.ts
@@ -0,0 +1,16 @@
+import { generateRooms } from '../utils'
+
+describe('generateRooms', () => {
+  it('Generates rooms with depth of 0', () => {
+    const dungeon = generateRooms({ depth: 0 })
+    expect(dungeon).toEqual([{ x: 0, y: 0 }])
+  })
+  it('Generates room with depth of 1', () => {
+    const dungeon = generateRooms({ depth: 1 })
+    expect(dungeon).toEqual([
+      { x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 }, 
+      { x: -1, y: 0 }, { x: 0, y: 0 }, { x: 1, y: 0 }, 
+      { x: -1, y: -1 }, { x: 0, y: -1 }, { x: 1, y: -1 }
+    ])
+  })
+})
---
+++ src/components/MapRenderer/utils.ts
@@ -0,0 +1,18 @@
+type Coords = {
+  x: number,
+  y: number,
+}
+
+export const generateRooms = ({ depth }: { depth: number }): Coords[] => {
+  var roomArray: Coords[] = [];
+
+  for (let yCoord = depth; yCoord >= -depth; yCoord--) {
+    for (let xCoord = -depth; xCoord <= depth; xCoord++) {
+      if (xCoord == -0) { xCoord = 0 }
+      if (yCoord == -0) { yCoord = 0 }
+      roomArray.push({ x: xCoord, y: yCoord })
+    }
+  }
+
+  return roomArray
+}
